9f1e6b0c479d81fadb33ecc8f01bcefde6b0a52d
ytsmm <82078728+ytsmm@users.noreply.github.com>
2023-06-05 01:18:27 +0500

    Add files via upload

--- website/listExample/script.js
+++ website/listExample/script.js
@@ -3,7 +3,9 @@ function getJson(url) {
         .then(result => result.json())
         .catch(error => {
             console.log(error);
-        })}    
+        });
+    }    
+
 function mounted(articles, clusters, quantity) {
     this.getJson('./data.json')
         .then(data => {
@@ -14,7 +16,9 @@ function mounted(articles, clusters, quantity) {
                 clusters.push(el);
             }
             quantity.push(data.clusterQuantity);
-    });}         
+    });
+}       
+
 const articles = [], clusters = [], quantity = [];
 mounted(articles, clusters, quantity);
 const clNumber = [], clKeywords = [], clQuantity = [];
@@ -51,17 +55,23 @@ setTimeout(() => {
         classHeader.setAttribute('id', `Class${i}`);
         let className = (clKeywords[i][0])[0].toUpperCase() + (clKeywords[i][0]).slice(1);
         classHeader.textContent = className;
-        classArticle.append(classHeader);   
+        classArticle.append(classHeader);  
+        let articleList = document.createElement('div');
+        articleList.setAttribute('class', 'articles');
         for (let article of articles) {
             if (article.class == i) {
-                let articleDiv = document.createElement('div');          
-                articleDiv.setAttribute('class', 'article');
-                let name = document.createElement('h4');
+                let articleDiv = document.createElement('div');   
+                if (articleList.children.length >= 5) {
+                    articleDiv.setAttribute('class', 'article disable');
+                } else {
+                    articleDiv.setAttribute('class', 'article');
+                }                       
+                let title = document.createElement('h4');
                 let nameLink = document.createElement('a');
                 nameLink.textContent = article.title;
                 nameLink.href = article.doi;
-                name.append(nameLink);
-                articleDiv.append(name);
+                title.append(nameLink);
+                articleDiv.append(title);
                 let authors = document.createElement('p');
                 authors.textContent = article.authors;
                 authors.setAttribute('class', 'data');
@@ -75,9 +85,29 @@ setTimeout(() => {
                 link.href = article.doi;           
                 articleLink.append(link);
                 articleDiv.append(articleLink);
-                classArticle.append(articleDiv);
+                articleList.append(articleDiv);
             }
-        }         
-        classes.appendChild(classArticle);}
+        }       
+
+        let btnMore = document.createElement('button');
+        btnMore.setAttribute('class', 'btnMore');
+        btnMore.textContent = "Show more";
+        btnMore.addEventListener("click", () => {
+            let list = btnMore.parentElement.querySelector('.articles');
+            if (list.children.length > 6) {
+                for (let i = 6; i < list.children.length; i++) {
+                    list.childNodes[i].classList.toggle('disable');
+                }
+            }
+            if (btnMore.textContent == 'Show more') {
+                btnMore.textContent = 'Hide';
+            } else {
+                btnMore.textContent = "Show more";
+            }
+        });
+        classArticle.append(articleList);
+        classArticle.append(btnMore);
+        classes.appendChild(classArticle);
+    }   
 }, 1000);
     
